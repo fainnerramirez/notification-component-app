dea699212ca3dcf9478658f380600ed81801ccb8
Fainner Ramírez <fainner.ramirez@dentsu.com>
2025-02-14 16:42:22 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+(
+    function(){
+        const loadNotifications = () => {
+            setTimeout(() => {
+                console.log("Notification run js");
+            }, 1000);
+        }
+        loadNotifications();        
+    }
+)()
